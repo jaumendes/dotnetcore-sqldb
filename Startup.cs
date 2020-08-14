6abbee4c47be164e48709b4cfe3c6a7d614ce897
Pedro Mendes <drito_16@hotmail.com>
2020-08-14 19:53:57 +0100

    Addicionei CORS policy e adicionei images dos produtos em wwwroot

--- Startup.cs
+++ Startup.cs
@@ -32,11 +32,18 @@ namespace DotNetCoreSqlDb
             services.AddControllersWithViews();
             services.AddDbContext<MyDatabaseContext>(options =>
                     options.UseSqlite("Data Source=foodlist08.db"));
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            }));
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
